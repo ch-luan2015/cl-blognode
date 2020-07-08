c02fbe62578c66ff78fbb4530e997d8d08401594
ch-luan2015 <45224298+ch-luan2015@users.noreply.github.com>
2020-07-08 19:05:12 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 let MovieStore = require('./movies.js')
-
 let movieStore = new MovieStore();
+
+//apply middleware by use app.use()
+
+app.use(bodyParser.json({
+    type: "application/json"
+}))
 
 //Direct / to /movies when user go web
 app.get('/', (req, res) => {
@@ -15,14 +21,75 @@ app.get('/movies', (req, res) => {
 })
 
 //Param title for find 
+app.get('/movies/:Title', (req, res) => {
 
-app.get('/movies/:Title/:year',(req, res) => {
-    console.log(req.params);
-    return res.send(req.params)
-    
+    //When one resquest find movie use res.params.title we use fucntion params do it
+    let foundMovie = movieStore.find(req.params.Title);
+
+    //check array length of foundmovie
+    (foundMovie.length < 1)
+        ? (
+            res.statusCode = 404,
+            res.send({
+                message: 'movie not found'
+            })
+        )
+        : res.send({
+            message: 'found movie',
+            payload: foundMovie.pop()
+        })
 })
 
-app.listen(8000, function () {
-    console.log('server started at : 127.0.0.1:8000');
+//Post to router movies
+app.post('/movies', (req, res) => {
+    //Client send req body , we should use 1 middleware convert data from string to json
+    console.log('req.body', req.body);
+
+    //Check data if user post error data : not title or title not correct 
+    if (!req.body.Title || req.body.Title.trim().length < 1) {
+        res.statusCode = 400; //bad input
+        res.send({
+            message:"Missing or Invalid Title"
+        })
+    }
+
+    //Check movie has on movieData , no need add 
+    if(movieStore.has(req.body.Title)){
+        res.statusCode = 400;
+        return res.send({
+            message: "Movie already existed"
+        });
+    }
+
+    movieStore.add(req.body);
+    res.statusCode = 200;
+    return res.send({
+        message: 'Movie added successfully'
+    })
 
 })
+
+
+//Update movie
+app.put('/movies', (req, res) => {
+    console.log('req.body', req.body);
+
+   
+
+    if(!movieStore.update(req.params.Title,req.body)){
+        res.statusCode = 500; 
+        return res.send({
+            message: 'Fail to update'
+        })
+    }
+    res.statusCode = 200;
+    return res.send({
+        message: 'Update successfully'
+    })
+
+})
+
+
+
+
+app.listen(5000,()=>{})
--- movies.js
+++ movies.js
@@ -2,15 +2,49 @@
 
 class MovieStore {
 
-    constructor(){
-        this.movieData=require('./movies.json')
-    
+    constructor() {
+        this.movieData = require('./movies.json')
     }
 
-    all(){
+    all() {
         return this.movieData;
+    }
+
+    find(Title){
+        return this.movieData.filter(m=>m.Title===Title)
+    }
+
+    add(movie){
+        let newMovie=movie;
+        return this.movieData.push(newMovie);
+
+    }
+
+    has(title){
+        let movie = this.find(title);
+
+        return movie.length > 0
+    }
+
+    update(title, newInfo){
+        //Check movie has , 0 return false
+
+        let movies = this.find(title);
+        if(movies.length<1){
+            return false;
+        }
+
+        let oldMovie= movies.pop();
+        let newMovie= Object.assign(oldMovie, newInfo);
+
+        let oldMoviesList = this.movieData.filter(m=>m.Title !== title);
+
+        this.movieData = [...oldMoviesList, newMovie];
+
+        return true;
+
     }
 
 }
 
-module.exports=MovieStore;
+module.exports = MovieStore;
