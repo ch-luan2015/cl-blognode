851cc449dc7f231be7c29a853daceacc8b2b89a3
ch-luan2015 <45224298+ch-luan2015@users.noreply.github.com>
2020-07-11 23:22:54 +0700

    query

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 let express = require('express');
-let app = express();
 let bodyParser = require('body-parser');
-let movieRouter = require('./Routes/movieRouter')
+let app = express();
+let movieRouter = require('./Routes/movieRouter');
+const db = require('./myapi/queries');
+const port = 5000 ;
 
 //apply middleware by use app.use()
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
 
 app.use(bodyParser.json({
     type: "application/json"
 }))
 
+app.get('/', (request, response) => {
+    response.json({Welcome: 'How to create API with Node.js,EXPRESS and PostgreSQL' })
+  })
+//End points
 
-//GET
+app.get('/countries', db.getCountries)
+app.get('/countries/:id', db.getCountryById)
+app.post('/countries', db.createCountry)
+app.put('/countries/:id', db.updateCountry)
+app.delete('/countries/:id', db.deleteCountry)//GET
 //Direct / to /movies when user go web
-app.get('/', (req, res) => {
-    return res.redirect('/movies')
-})
+// app.get('/', (req, res) => {
+//     return res.redirect('/movies')
+// })
 
-app.use('/movies',movieRouter);
+// app.use('/movies',movieRouter);
  
-app.listen(5000, () => { })
+app.listen(port, () => { })
